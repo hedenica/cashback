555a54808cf86e8d40a6c9189ea5b6f0946b512d
hedenicapedrosa <hedenica@hotmail.com>
2021-01-07 22:03:17 -0300

    🚧  Add SignUp view - wip

---
+++ src/SignUp/views/SignUp.jsx
@@ -0,0 +1,59 @@
+import React, { useRef } from 'react';
+import { Form } from '@unform/web';
+import { LogIn } from 'react-feather';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+import Button from '../../App/components/Button';
+import Input from '../../App/components/Input';
+
+import logoImg from '../../assets/img/logo.png';
+
+const SignUp = () => {
+  const formRef = useRef(null);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <AnimationContainer>
+          <img src={logoImg} alt="O Boticário" />
+          <Form ref={formRef} onSubmit={() => console.log('submit!')}>
+            <h1>Faça seu Cadastro</h1>
+            <Input
+              name="name"
+              type="text"
+              autoComplete="username"
+              placeholder="Digite seu nome completo"
+              required
+            />
+            <Input
+              name="email"
+              type="email"
+              autoComplete="username"
+              placeholder="Digite seu e-mail"
+              required
+            />
+            <Input
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              placeholder="Digite sua senha"
+              required
+            />
+            <Button type="submit">Entrar</Button>
+          </Form>
+
+          <p>Já tem cadastro ? </p>
+          <Link to="/">
+            <LogIn />
+            Clique aqui para logar!
+          </Link>
+        </AnimationContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
